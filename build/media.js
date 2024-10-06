6d00dd5cf0bdad66e418344986d7c2353cc386f7
Gustavo Andre <gugamathiaz12@gmail.com>
2024-10-06 18:55:31 -0300

    iniciando projeto ts

---
+++ build/media.js
@@ -0,0 +1,50 @@
+"use strict";
+const primeiraNota = Math.floor(Math.random() * 10);
+const segundaNota = Math.floor(Math.random() * 10);
+const terceiraNota = Math.floor(Math.random() * 10);
+function media() {
+    const total = (primeiraNota + segundaNota + terceiraNota) / 3;
+    let nome;
+    switch (true) {
+        case total <= 1:
+            logger("Pedro", total);
+            break;
+        case total <= 2:
+            logger("Gustavo", total);
+            break;
+        case total <= 3:
+            logger("Luiz", total);
+            break;
+        case total <= 4:
+            logger("Ruan", total);
+            break;
+        case total <= 5:
+            logger("Rian", total);
+            break;
+        case total <= 6:
+            logger("Henrique", total);
+            break;
+        case total <= 7:
+            logger("Lucas", total);
+            break;
+        case total <= 8:
+            logger("Joao", total);
+            break;
+        case total <= 9:
+            logger("Vitor", total);
+            break;
+        case total <= 10:
+            logger("Bernado", total);
+            break;
+        default:
+            break;
+    }
+}
+function logger(nome, total) {
+    if (total <= 6) {
+        console.log(`Aluno: ${nome} está reprovado na final com nota ${total.toFixed(1)}.`);
+        return;
+    }
+    console.log(`Aluno: ${nome} está aprovado na final com nota ${total.toFixed(1)}.`);
+}
+setTimeout(() => { media(); }, 1500);
