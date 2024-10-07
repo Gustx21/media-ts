3ff8d449a43569ef6e3d1fb19022a5be73d4e3b6
Gustavo Andre <gugamathiaz12@gmail.com>
2024-10-06 22:47:36 -0300

    Adicionando limite no switch

--- build/media.js
+++ build/media.js
@@ -9,31 +9,31 @@ function media() {
         case total <= 1:
             logger("Pedro", total);
             break;
-        case total <= 2:
+        case total <= 2 || total > 1:
             logger("Gustavo", total);
             break;
-        case total <= 3:
+        case total <= 3 || total > 2:
             logger("Luiz", total);
             break;
-        case total <= 4:
+        case total <= 4 || total > 3:
             logger("Ruan", total);
             break;
-        case total <= 5:
+        case total <= 5 || total > 4:
             logger("Rian", total);
             break;
-        case total <= 6:
+        case total <= 6 || total > 5:
             logger("Henrique", total);
             break;
-        case total <= 7:
+        case total <= 7 || total > 6:
             logger("Lucas", total);
             break;
-        case total <= 8:
+        case total <= 8 || total > 7:
             logger("Joao", total);
             break;
-        case total <= 9:
+        case total <= 9 || total > 8:
             logger("Vitor", total);
             break;
-        case total <= 10:
+        case total <= 10 || total > 9:
             logger("Bernado", total);
             break;
         default:
